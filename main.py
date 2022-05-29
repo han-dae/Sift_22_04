246a73e6098ffbeb1fef5aef59832a2736053c45
dae <h33223012@naver.com>
2022-05-30 01:33:05 +0900

    Sift_project 22/04

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+img1 = cv2.imread('mydog.jpg',0)
+img2 = cv2.imread('myhome.jpeg',0)
+gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
+# SIFT 서술자 추출기 생성 ---①
+detector = cv2.xfeatures2d.SIFT_create()
+# 각 영상에 대해 키 포인트와 서술자 추출 ---②
+kp1, desc1 = detector.detectAndCompute(gray1, None)
+kp2, desc2 = detector.detectAndCompute(gray2, None)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# BFMatcher 생성, L1 거리, 상호 체크 ---③
+matcher = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+# 매칭 계산 ---④
+matches = matcher.match(desc1, desc2)
+# 매칭 결과 그리기 ---⑤
+res = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, \
+                      flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+# 결과 출력
+cv2.imshow('BFMatcher + SIFT', res)
+cv2.waitKey()
+cv2.destroyAllWindows()
